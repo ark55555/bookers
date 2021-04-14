782a59ee87819c0ef3e30612a27b924f3c455fa7
ark55555 <ark.tooyoung@gmail.com>
2021-04-14 10:57:03 +0000

    [Add]3-8validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice:  'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -21,14 +25,18 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    end
   end
 
 
